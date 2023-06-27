9288aef7d528658a7e3a991efa3723981bf994df
StSapunkov <105738333+StSapunkov@users.noreply.github.com>
2023-06-27 23:38:44 +0300

    4-2-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
+import { useState } from 'react';
+import { GlobalStyle } from './Layout/GlobalStyle';
+import { Container } from './Layout/Container.styled';
+import { Filter } from './Contacts/ContactFilter/Filter';
+import { ContactForm } from './Contacts/ContactForm/ContactForm';
+import { ContactList } from './Contacts/ContactList/ContactList';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+
 export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addNewContacts = (newContact, { action }) => {
+    const duplicateName = contacts.find(
+      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    return (
+      duplicateName
+        ? alert(`${newContact.name} is already in contacts.`)
+        : setContacts(prevContacts => [...prevContacts, newContact]),
+      action.resetForm()
+    );
+  };
+
+  const onDeleteContacts = id => {
+    setContacts(prevContacts => prevContacts.filter(el => el.id !== id));
+  };
+
+  const changeFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  const getVisibleContact = () => {
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addNewContacts} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContact()}
+        deleteContact={onDeleteContacts}
+      />
+      <GlobalStyle />
+    </Container>
   );
 };
---
+++ src/components/Layout/Container.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0;
+  padding: 20px;
+
+  border-radius: 20px;
+
+  background-color: #0c3079;
+`;
